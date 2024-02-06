badec7d69e14789b1f91eb2846e8859bbc185b9b
peixed351 <peixed351@gmail.com>
2024-02-06 17:56:17 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "BUP20"
 PROJECT_NAME_FULL: str = "BUP20: Sweet Pepper Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
